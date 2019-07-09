14bc61c6fcd9733422891992d136bc27f5422c06
github username <github email address>
2019-07-09 14:32:02 -0400

    Main Deliverables Working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // form tag
+  const formTag = document.querySelector("form")
+  // input box tag
+  const inputTag = document.querySelector("input")
+
+  // ADD TASKS
+
+  formTag.addEventListener("submit", function(e) {
+    // prevent default behavior
+    e.preventDefault();
+    // grabs text from input value and stores in variable
+    const toDo = inputTag.value
+    // finds ul tag
+    const ulTag = document.querySelector("ul")
+    // makes new list element
+    const newLi = document.createElement('li')
+
+    // changes inner text to todo, then appends to document ul tag
+    newLi.innerText = toDo
+    ulTag.appendChild(newLi)
+  })
+
+
 });
